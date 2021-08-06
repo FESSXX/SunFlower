aef8f0174fa7cf74fe30debb4140d5c2ea56ec7c
FESSXX <2821647050@qq.com>
2021-08-06 18:02:20 +0800

    3.1

--- js/show.js
+++ js/show.js
@@ -56,7 +56,18 @@ vue = new Vue({
 		   },
 		   init(){
 		         this.fetchData()
-		    }
+		    },
+			gyw(id){
+				if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+				   this.$notify.error({
+				            title: '',
+				            message: '暂不支持手机端哦'
+				          });
+				  } else {
+				    this.isrt=id;
+				  }
+				 
+			}
 	},
 	mounted(){
 		this.init();
